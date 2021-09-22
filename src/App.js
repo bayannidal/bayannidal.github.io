0feb4a330c021c11ce190b04a1fb29738e51822c
Nidal Bayan <bayan_nidal@yahoo.com>
2021-09-22 21:32:45 +0300

    a

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
       setTheme('light-theme')
       setChecked(true)
       localStorage.setItem('theme', 'light-theme')
-      document.querySelector('meta[name="theme-color"]').setAttribute('content', '#FFEECA')
+      document.querySelector('meta[name="theme-color"]').setAttribute('content', '#e4cfa9')
 
     }
   }
